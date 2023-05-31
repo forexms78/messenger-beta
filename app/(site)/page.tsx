2b38a5a8218ab5d8c907fc82ab35bf57f9936acc
forexms78 <49295076+forexms78@users.noreply.github.com>
2023-06-01 02:27:12 +0900

    logo size change

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <div className="sm:mx-auto  sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
-          height="48"
-          width="48"
+          height="90"
+          width="90"
           className="mx-auto w-auto"
           src="/images/logo.png"
         />
